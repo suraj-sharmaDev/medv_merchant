f6c76bc301292eda15fbf7d4e29a2014beaccc3f
suraj-sharmaDev <surajdroid97@gmail.com>
2020-05-28 20:18:26 +0530

    Corrected logic

--- profile.php
+++ profile.php
@@ -2,10 +2,13 @@
 </head>
 <body>
 	<?php include('_includes/navbar.php'); ?>
-	<div id="info-div">
+	<div class="container pt-3" id="info-div">
 		<!-- contains the merchant info -->
 		<!-- autofilled by js -->
 	</div>
 </body>
 <script type="module" src="./scripts/profile.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" crossorigin="anonymous"></script>
+<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
 </html>
