43ba225eb2965c00c97e89fdc541293085ab4c58
suraj-sharmaDev <surajdroid97@gmail.com>
2020-05-30 14:29:59 +0530

    Almost

--- quoteOrder.php
+++ quoteOrder.php
@@ -16,7 +16,7 @@
 		if(!$apiOrder && !$apiConfig){
 			echo 'There was an issue in server side!. Please contact administrator';
 		}else{
-			$config = json_decode($apiConfig, true);
+			$config = json_decode($apiConfig, true); 
 			$data = json_decode($apiOrder, true)['liOrdDtls'];
 
 ?>
@@ -27,24 +27,27 @@
 	<div class="container-fluid pt-3" id="info-div">
 		<!-- contains the order info -->
 		<form action="">
+			<input type="hidden" id="appConfig" value='<?php echo $apiConfig;?>' />
+			<input type="hidden" name="orderId" id="orderId" value="<?php echo $orderId; ?>" />			
 			<?php
 				//iterate over the medicines got from api above
 				foreach ($data as $key => $value) {
 			?>
-			<div class="form-group">
-				<input type="hidden" name="OrderDtls_Id" id="OrderDtls_Id" value="<?php echo $value['OrderDtls_Id']; ?>" />
+			<div class="form-group medicines">
+				<input type="hidden" class="OrderDtls_Id" value="<?php echo $value['OrderDtls_Id']; ?>" />
+				<input type="hidden" class="Order_Qty" value="<?php echo $value['Order_Qty']; ?>" />
 				<p><?php echo $value['MedicineName']; ?></p>
 				<div class="row">
 					<div class="col-2">
-						<?php echo $value['Order_Qty'].' '.'No'; ?>						
+						<?php echo $value['Order_Qty'].' '.'No'; ?>			
 					</div>					
 					<div class="col-5">
 						<label for="mrp">MRP Price</label>
-						<input type="number" placeholder="0" class="form-control" id="mrp"/>
+						<input type="number" placeholder="0" class="form-control" class="mrp" onchange="changeMrp(this);" />
 					</div>
 					<div class="col-5">
 						<label for="sp">Selling Price</label>
-						<input type="number" placeholder="0" class="form-control" id="sp"/>
+						<input type="number" placeholder="0" class="form-control" class="sp" onchange="changeSp(this);" />
 					</div>					
 				</div>
 			</div>
@@ -54,34 +57,37 @@
 			<!-- final bill for the order -->
 			<div class="w-50 mx-auto form-group d-flex justify-content-between">
 				<span>Sub Total : </span>
-				<span id="sub-total">100</span>
+				<span id="sub-total"></span>
 			</div>
 			<div class="w-50 mx-auto form-group d-flex justify-content-between">
 				<span>10% discount : </span>
-				<span id="discount">100</span>
+				<span id="discount"></span>
 			</div>
 			<div class="w-50 mx-auto form-group d-flex justify-content-between">
 				<span>Merchant Off : </span>
-				<span id="merchant-off">100</span>
+				<input type="number" placeholder="0" class="mrp" onchange="merchantDisc(this);" value="0" />
 			</div>
 			<div class="w-50 mx-auto form-group"><hr/></div>
 			<div class="w-50 mx-auto form-group d-flex justify-content-between">
 				<span><?php echo $config['GST']; ?>% GST : </span>
-				<span id="gst">100</span>
+				<span id="gst"></span>
 			</div>			
 			<div class="w-50 mx-auto form-group d-flex justify-content-between">
 				<span>Service Charge : </span>
-				<span id="service-charge">100</span>
+				<span id="service-charge"><?php echo $config['stdShipping']; ?></span>
 			</div>
 			<div class="w-50 mx-auto form-group"><hr/></div>
 			<div class="w-50 mx-auto form-group d-flex justify-content-between">
 				<span>Total : </span>
-				<span id="total">100</span>
-			</div>			
+				<span id="total"></span>
+			</div>
+			<div class="w-50 mx-auto form-group">
+				<button type="button" class="btn btn-primary" onclick="quoteOrder();">Submit</button>				
+			</div>		
 		</form>
 	</div>
 </body>
-<script type="module" src="./scripts/quoteOrder.js"></script>
+<script type="module" src="./scripts/quoteOrder.js?v=<?php echo(rand(10,100)); ?>"></script>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" crossorigin="anonymous"></script>
 <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
--- scripts/quoteOrder.js
+++ scripts/quoteOrder.js
@@ -1,12 +1,90 @@
-console.log('loaded');
-
-
 //function call for quoteOrder
-$('#mrp').change(function(){
-	console.log($(this).val())
-})
+var orderId = $('#orderId').val();
+var appConfig = JSON.parse($('#appConfig').val());
 
+var formData = {
+	Order_Id : parseInt(orderId),
+	MerchId : parseInt(_localStorage.merchId),
+	SubTotal : 0,
+	Discount : 0,
+	OtherOff : 0,
+	DiscountPercentage : 10,
+	GST : 0,
+	GSTpercentage : parseInt(appConfig.GST),
+	Shipping : parseInt(appConfig.stdShipping),
+	Total : 0,
+	liOrdDtls : []
+};
+
+const updateBill = () => {
+	//best way is to save old value in changeSp()
+	//and do the subtotal calculation thereitself
+	var subTotal = 0;
+	formData.liOrdDtls.map((e)=>{
+		subTotal += parseInt(e.Amt)
+	});
+	formData.SubTotal = subTotal;
+	formData.GST = parseInt((formData.GSTpercentage * 0.01 * subTotal).toFixed(1));
+	formData.Discount = parseInt((formData.DiscountPercentage * 0.01 * subTotal).toFixed(1));
+	formData.Total = formData.SubTotal + formData.GST + formData.Shipping - formData.Discount - formData.OtherOff;
+	$('#sub-total').html(subTotal);
+	$('#discount').html(formData.Discount);
+	$('#gst').html(formData.GST);
+	$('#total').html(formData.Total);	
+}
+
+//intial loop through all medicines
+$(".medicines").each(function() {
+    // values.push($(this).val());
+    // console.log($(this).children());
+    var pushData = {
+    	OrderDtls_Id : $(this).find('.OrderDtls_Id').val(),
+    	Order_Qty : $(this).find('.Order_Qty').val(),
+    	Amt : $(this).find('.sp').val() ? parseInt($(this).find('.sp').val()) : 0,
+    	MRP : $(this).find('.mrp').val() ? parseInt($(this).find('.mrp').val()) : 0
+    }
+    formData.liOrdDtls.push(pushData);
+});
+//initial bill configuration
+updateBill();
+console.log(formData);
+
+window.changeMrp = function (el){
+	var parent = $(el).closest('.medicines');
+	var OrderDtls_Id = $(parent).find('.OrderDtls_Id').val();
+	var index = formData.liOrdDtls.findIndex((v)=>v.OrderDtls_Id==OrderDtls_Id);
+	var updated = formData.liOrdDtls[index];
+	updated.MRP = parseInt(el.value); 
+	formData.liOrdDtls[index] = updated;
+}
+
+window.changeSp = function (el){
+	var parent = $(el).closest('.medicines');
+	var OrderDtls_Id = $(parent).find('.OrderDtls_Id').val();
+	var index = formData.liOrdDtls.findIndex((v)=>v.OrderDtls_Id==OrderDtls_Id);
+	var updated = formData.liOrdDtls[index];
+	updated.Amt = parseInt(el.value); 
+	formData.liOrdDtls[index] = updated;
+	//after updating formdata we have to make necessary updates in
+	//bill too
+	updateBill();
+}
+
+window.merchantDisc =  function (el){
+	formData.OtherOff = el.value ? parseInt(el.value) : 0;
+	updateBill();
+}
 
 window.quoteOrder = function(){
-
+	var count = 5;
+	$('input[type="number"]').each(function(){
+	   if($(this).val() !=""){
+	      count --;
+	    }
+	 });
+	 if(count == 0){
+	 	console.log('submitting');
+	 }else{
+	 	alert('Fill all the fields');
+	 }
 }
